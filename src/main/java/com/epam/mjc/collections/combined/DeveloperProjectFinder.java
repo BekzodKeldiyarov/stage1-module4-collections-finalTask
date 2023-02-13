4633c3db837bcd79f1b951c092e99643879451b5
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-13 19:12:48 +0500

    Done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,33 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+        List<String> data = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                data.add(entry.getKey());
+            }
+        }
+
+        Collections.sort(data, new CustomSortMethod());
+
+        return data;
+    }
+}
+
+class CustomSortMethod implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length() > o2.length()) {
+            return -1;
+        } else if (o1.length() < o2.length()) {
+            return 1;
+        } else {
+            int i = o1.compareTo(o2) * (-1);
+            return i;
+        }
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> data = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            int key = entry.getKey().length();
+            int stringLength = entry.getKey().length();
+            Set<String> values;
+            if (data.containsKey(stringLength)) {
+                values = data.get(stringLength);
+            } else {
+                values = new HashSet<>();
+            }
+            values.add(entry.getKey());
+            data.put(stringLength, values);
+        }
+        return data;
     }
 }
